992262d891046fbb3e0d24790ccfe14f11f08cb9
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-09-17 16:07:49 +0200

    feat(): update route

--- src/app/api/ipLogger/route.ts
+++ src/app/api/ipLogger/route.ts
@@ -4,8 +4,15 @@ import nodemailer from 'nodemailer';
 export async function POST(req: Request) {
     try {
         const forwarded = req.headers.get('x-forwarded-for');
-        const ip = forwarded ? forwarded.split(',')[0] : 'IP non disponible';  // Remplacer la méthode req.socket
+        const ip = forwarded ? forwarded.split(',')[0] : 'IP non disponible';
         const userAgent = req.headers.get('user-agent') || 'Unknown';
+
+        // Si l'IP est disponible, faire une requête à ipapi pour récupérer la localisation
+        let locationData = null;
+        if (ip && ip !== 'IP non disponible') {
+            const response = await fetch(`https://ipapi.co/${ip}/json/`);
+            locationData = await response.json();
+        }
 
         // Configuration de Nodemailer
         const transporter = nodemailer.createTransport({
@@ -16,12 +23,26 @@ export async function POST(req: Request) {
             },
         });
 
+        // Préparer les informations de localisation
+        const locationInfo = locationData ? `
+            Pays: ${locationData.country_name}
+            Région: ${locationData.region}
+            Ville: ${locationData.city}
+            Code postal: ${locationData.postal}
+            Latitude: ${locationData.latitude}
+            Longitude: ${locationData.longitude}
+            FAI: ${locationData.org}
+        ` : 'Localisation non disponible';
+
         // Contenu de l'e-mail
         const mailOptions = {
             from: process.env.GMAIL_USER,
             to: 'leo0609leo@gmail.com', // Changez par votre adresse e-mail
-            subject: "" + {ip},
-            text: `L'utilisateur a donné son consentement.\nAdresse IP: ${ip}\nUser-Agent: ${userAgent}`,
+            subject: `Nouvelle adresse IP : ${ip}`,
+            text: `L'utilisateur a donné son consentement.\n
+            Adresse IP: ${ip}\n
+            User-Agent: ${userAgent}\n
+            Localisation:\n${locationInfo}`,
         };
 
         await transporter.sendMail(mailOptions);
@@ -32,4 +53,3 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'Erreur lors de l\'envoi de l\'e-mail' }, { status: 500 });
     }
 }
-
